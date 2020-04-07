0b2419ffa689e9ee9e71ba73e7142801f109b571
Oriol Villanova <62610722+oriol-v-ll@users.noreply.github.com>
2020-04-07 17:18:42 +0200

    Creación v1.0 de slpash activity

--- app/src/main/java/cat/urv/deim/asm/p2/common/SplashActivity.java
+++ app/src/main/java/cat/urv/deim/asm/p2/common/SplashActivity.java
@@ -1,14 +1,31 @@
 package cat.urv.deim.asm.p2.common;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.os.Handler;
 
 import android.os.Bundle;
+import android.view.WindowManager;
 
-public class SplashActivity extends AppCompatActivity {
+public class SplashActivity extends Activity {
+
+    private final int DURACION_SPLASH = 3000;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable(){
+            public void run(){
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            };
+        }, DURACION_SPLASH);
     }
 }
