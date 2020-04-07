78046e99b525143639276ba7a65eb1fd61c71fc7
Oriol Villanova <62610722+oriol-v-ll@users.noreply.github.com>
2020-04-07 17:33:41 +0200

    1.1

--- app/src/main/java/cat/urv/deim/asm/p2/common/MainActivity.java
+++ app/src/main/java/cat/urv/deim/asm/p2/common/MainActivity.java
@@ -26,15 +26,7 @@ public class MainActivity extends AppCompatActivity {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
 
-                //system.out.println("hola"); comento el hola paa ver si me a el git y pongo adios
-                system.out.println("deu Uri");
-                //esto es otra prueba
-                void hola(){
 
-                }
-
-
-                //prueba
             }
         });
     }
