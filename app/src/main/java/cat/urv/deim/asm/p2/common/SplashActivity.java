444474a53014ddbca26ec12cd46a7ba948bdb18a
pjuanma <63060206+pjuanma@users.noreply.github.com>
2020-04-09 12:23:12 +0200

    Tuorial entra una vez

--- app/src/main/java/cat/urv/deim/asm/p2/common/SplashActivity.java
+++ app/src/main/java/cat/urv/deim/asm/p2/common/SplashActivity.java
@@ -1,7 +1,9 @@
 package cat.urv.deim.asm.p2.common;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.ActivityInfo;
 import android.os.Handler;
 
@@ -21,13 +23,34 @@ public class SplashActivity extends AppCompatActivity {
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         setContentView(R.layout.activity_splash);
+        //Declaramos bandera para que entre una sola vez en el tutorial
+        SharedPreferences sharedPref = this.getPreferences(Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPref.edit();
+        boolean tutorialDone = sharedPref.getBoolean("TUTORIAL_DONE", true);
 
-        new Handler().postDelayed(new Runnable(){
-            public void run(){
-                Intent intent = new Intent(SplashActivity.this, TutorialActivity.class);
-                startActivity(intent);
-                finish();
-            };
-        }, DURACION_SPLASH);
+        if (tutorialDone) {
+            //Cambiamos a false despues de que entre por primera vez en el tutorial, ya no entrara mas
+            editor.putBoolean("TUTORIAL_DONE", false);
+            editor.commit();
+            new Handler().postDelayed(new Runnable() {
+                public void run() {
+                    Intent intent = new Intent(SplashActivity.this, TutorialActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+
+                ;
+            }, DURACION_SPLASH);
+        }else {
+            new Handler().postDelayed(new Runnable() {
+                public void run() {
+                    Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+
+                ;
+            }, DURACION_SPLASH);
+        }
     }
 }
