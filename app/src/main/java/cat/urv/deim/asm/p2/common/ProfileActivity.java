f2e25226064e854497d2395a6e04332d3ed1b218
Oriol Villanova <62610722+oriol-v-ll@users.noreply.github.com>
2020-04-11 20:53:37 +0200

    comentari

--- app/src/main/java/cat/urv/deim/asm/p2/common/ProfileActivity.java
+++ app/src/main/java/cat/urv/deim/asm/p2/common/ProfileActivity.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class ProfileActivity extends AppCompatActivity {
-
+//
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
