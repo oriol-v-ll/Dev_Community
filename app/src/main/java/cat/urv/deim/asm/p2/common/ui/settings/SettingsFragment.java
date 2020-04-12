f8c60799148a4a252b0e5bfe9f97b3838f6b991a
Oriol Villanova <62610722+oriol-v-ll@users.noreply.github.com>
2020-04-12 17:59:04 +0200

    Arreglo menú.(lineas)
     -  Los 3 puntitos no se puede redirigir porque es un fragment, es un poco movida el tema

--- app/src/main/java/cat/urv/deim/asm/p2/common/ui/settings/SettingsFragment.java
+++ app/src/main/java/cat/urv/deim/asm/p2/common/ui/settings/SettingsFragment.java
@@ -1,5 +1,6 @@
 package cat.urv.deim.asm.p2.common.ui.settings;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,6 +13,8 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import cat.urv.deim.asm.p2.common.LoginActivity;
+import cat.urv.deim.asm.p2.common.MainActivity;
 import cat.urv.deim.asm.p2.common.R;
 
 public class SettingsFragment extends Fragment{
@@ -30,5 +33,10 @@ public class SettingsFragment extends Fragment{
             }
         });
         return root;
+    }
+
+    public void settings(View view){
+        //Mover al settings
+
     }
 }
