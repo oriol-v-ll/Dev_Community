ffe0058b3875a03e511448416b2857732e427777
Oriol Villanova <62610722+oriol-v-ll@users.noreply.github.com>
2020-04-16 16:51:41 +0200

    Funciona cambiar perfil al activity

--- app/src/main/java/cat/urv/deim/asm/p2/common/MainActivity.java
+++ app/src/main/java/cat/urv/deim/asm/p2/common/MainActivity.java
@@ -11,6 +11,7 @@ import android.view.MenuItem;
 import android.view.View;
 
 import androidx.core.view.GravityCompat;
+import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -26,6 +27,14 @@ import androidx.appcompat.widget.Toolbar;
 
 import android.view.Menu;
 import android.widget.Button;
+
+import cat.urv.deim.asm.p2.common.ui.articles.ArticlesFragment;
+import cat.urv.deim.asm.p2.common.ui.bookmarks.BookmarksFragment;
+import cat.urv.deim.asm.p2.common.ui.calendar.CalendarFragment;
+import cat.urv.deim.asm.p2.common.ui.events.EventsFragment;
+import cat.urv.deim.asm.p2.common.ui.faqs.FaqsFragment;
+import cat.urv.deim.asm.p2.common.ui.favorites.FavoritesFragment;
+import cat.urv.deim.asm.p2.common.ui.news.NewsFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -56,7 +65,8 @@ public class MainActivity extends AppCompatActivity {
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        final DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        //seleciona la vista del menú lateral.
         NavigationView navigationView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
@@ -65,12 +75,78 @@ public class MainActivity extends AppCompatActivity {
                 R.id.nav_calendar)
                 .setDrawerLayout(drawer)
                 .build();
+        //Se pone los fragments correspondientes.
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
 
+        /*---------Control del botón que se haga en el menú---------------*/
+
+        navigationView.setNavigationItemSelectedListener( //Ponemos un listener en el navigationview
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+
+                        boolean fragmentTransaction = false;
+                        Fragment fragment = null;
+
+                        //
+                        switch (menuItem.getItemId()) {
+                            case R.id.nav_profile:
+                                Intent intent = new Intent(MainActivity.this, ProfileActivity.class);
+                                startActivity(intent);
+                                finish();
+                                break;
+                            case R.id.nav_news:
+                                fragment = new NewsFragment();
+                                fragmentTransaction = true;
+                                break;
+                            case R.id.nav_articles:
+                                fragment = new ArticlesFragment();
+                                fragmentTransaction = true;
+                                break;
+                            case R.id.nav_events:
+                                fragment = new EventsFragment();
+                                fragmentTransaction = true;
+                                break;
+                            case R.id.nav_calendar:
+                                fragment = new CalendarFragment();
+                                fragmentTransaction = true;
+                                break;
+
+                            case R.id.nav_faqs:
+                                fragment = new FaqsFragment();
+                                fragmentTransaction = true;
+                                break;
+                            case R.id.nav_favorites:
+                                fragment = new FavoritesFragment();
+                                fragmentTransaction = true;
+                                break;
+                            case R.id.nav_booksmarks:
+                                fragment = new BookmarksFragment();
+                                fragmentTransaction = true;
+                                break;
+                        }
+
+                        if(fragmentTransaction) {
+                            getSupportFragmentManager().beginTransaction()
+                                    .replace(R.id.nav_host_fragment, fragment)
+                                    .commit();
+
+                            menuItem.setChecked(true);
+                            getSupportActionBar().setTitle(menuItem.getTitle());
+                        }
+
+                        drawer.closeDrawers();
+
+                        return true;
+                    }
+                });
+        /*---------Fin control del botón que se haga en el menú---------------*/
+
 
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
