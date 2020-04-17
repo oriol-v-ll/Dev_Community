8209f35b8abf69ed63bea4af38a8810d06671151
Oriol Villanova <62610722+oriol-v-ll@users.noreply.github.com>
2020-04-17 16:06:54 +0200

    Revert "Falta arreglar ciertas activities"
    This reverts commit 88d77a8b

--- app/src/main/java/cat/urv/deim/asm/p2/common/SettingsActivity.java
+++ app/src/main/java/cat/urv/deim/asm/p2/common/SettingsActivity.java
@@ -1,16 +1,8 @@
 package cat.urv.deim.asm.p2.common;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import androidx.drawerlayout.widget.DrawerLayout;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
-
-import com.google.android.material.navigation.NavigationView;
 
 public class SettingsActivity extends AppCompatActivity {
 
@@ -18,12 +10,5 @@ public class SettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
-
-
-
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-
     }
 }
