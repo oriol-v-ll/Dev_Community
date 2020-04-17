26cd80ebb583b73160d715d362a7d78d3252cf60
Oriol Villanova <62610722+oriol-v-ll@users.noreply.github.com>
2020-04-17 16:06:14 +0200

    Revert "Arreglar faqs"
    This reverts commit ddd5344a

--- app/src/main/java/cat/urv/deim/asm/p2/common/FaqsActivity2.java
+++ app/src/main/java/cat/urv/deim/asm/p2/common/FaqsActivity2.java
@@ -1,7 +1,6 @@
 package cat.urv.deim.asm.p2.common;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
@@ -11,8 +10,5 @@ public class FaqsActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_faqs2);
-
-        Toolbar toolbar = findViewById(R.id.toolbar_profile);
-        setSupportActionBar(toolbar);
     }
 }
