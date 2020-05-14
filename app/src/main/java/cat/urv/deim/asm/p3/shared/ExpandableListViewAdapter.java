e6f29b7ff60b789374f8e86d29a9b456c400203a
Oriol Villanova <62610722+oriol-v-ll@users.noreply.github.com>
2020-05-14 19:20:19 +0200

    Expandable ListView para las FAQs

---
+++ app/src/main/java/cat/urv/deim/asm/p3/shared/ExpandableListViewAdapter.java
@@ -0,0 +1,98 @@
+package cat.urv.deim.asm.p3.shared;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseExpandableListAdapter;
+import android.widget.TextView;
+
+import java.util.HashMap;
+import java.util.List;
+
+import cat.urv.deim.asm.p2.common.R;
+
+public class ExpandableListViewAdapter extends BaseExpandableListAdapter {
+
+
+    private Context context;
+    private List<String> chapterList;
+    private HashMap<String, List<String>> topicsList;
+
+    public ExpandableListViewAdapter(Context context, List<String> chapterList, HashMap<String, List<String>> topicsList) {
+        this.context = context;
+        this.chapterList = chapterList;
+        this.topicsList = topicsList;
+    }
+
+
+    @Override
+    public int getGroupCount() {
+        return this.chapterList.size();
+    }
+
+    @Override
+    public int getChildrenCount(int groupPosition) {
+        return this.topicsList.get(this.chapterList.get(groupPosition)).size();
+    }
+
+    @Override
+    public Object getGroup(int groupPosition) {
+        return this.chapterList.get(groupPosition);
+    }
+
+    @Override
+    public Object getChild(int groupPosition, int childPosition) {
+        return this.topicsList.get(this.chapterList.get(groupPosition)).get(childPosition);
+    }
+
+    @Override
+    public long getGroupId(int groupPosition) {
+        return groupPosition;
+    }
+
+    @Override
+    public long getChildId(int groupPosition, int childPosition) {
+        return childPosition;
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return false;
+    }
+
+    @Override
+    public View getGroupView(int groupPosition, boolean isExpanded, View convertView, ViewGroup parent) {
+        String chapterTitle = (String) getGroup(groupPosition);
+
+        if (convertView==null){
+            LayoutInflater inflater = (LayoutInflater)this.context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = inflater.inflate(R.layout.chapter_list,null);
+        }
+
+        TextView chapterTv = convertView.findViewById(R.id.chapter_tv);
+        chapterTv.setText(chapterTitle);
+
+        return convertView;
+    }
+
+    @Override
+    public View getChildView(int groupPosition, int childPosition, boolean isLastChild, View convertView, ViewGroup parent) {
+        String topictitle = (String) getChild(groupPosition,childPosition);
+
+        if (convertView==null){
+            LayoutInflater inflater = (LayoutInflater)this.context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = inflater.inflate(R.layout.topics_list,null);
+        }
+
+        TextView topicTv = convertView.findViewById(R.id.topics_tv);
+        topicTv.setText(topictitle);
+
+        return convertView;
+    }
+
+    @Override
+    public boolean isChildSelectable(int groupPosition, int childPosition) {
+        return true;
+    }
+}
--- app/src/main/java/cat/urv/deim/asm/p3/shared/FaqsActivity.java
+++ app/src/main/java/cat/urv/deim/asm/p3/shared/FaqsActivity.java
@@ -6,16 +6,38 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
+import android.widget.ExpandableListView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 import cat.urv.deim.asm.p2.common.MainActivity;
 import cat.urv.deim.asm.p2.common.R;
 
 public class FaqsActivity extends MainActivity {
 
+    ExpandableListViewAdapter listViewAdapter;
+    ExpandableListView expandableListView;
+    List<String> chapterList;
+    HashMap<String, List<String>> topicList;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_faqs);
+
+
+        /*Creacio d'adapter*/
+        expandableListView = findViewById(R.id.eListView);
+        showList();
+
+        listViewAdapter = new ExpandableListViewAdapter(this, chapterList, topicList);
+        expandableListView.setAdapter(listViewAdapter);
+
+
+
+
         Toolbar toolbar = findViewById(R.id.toolbar_faqs);
         setSupportActionBar(toolbar);
         //El metodo setDiplayHomeUpEnabled pone la flecha pero al hacer click lo quita de cualquier elemento UI.
@@ -30,6 +52,51 @@ public class FaqsActivity extends MainActivity {
                 finish();
             }
         });
+    }
+
+    private void showList() {
+
+        chapterList = new ArrayList<String>();
+        topicList = new HashMap<String, List<String>>();
+
+        chapterList.add("Chapter 1");
+        chapterList.add("Chapter 2");
+        chapterList.add("Chapter 3");
+        chapterList.add("Chapter 4");
+        chapterList.add("Chapter 5");
+
+        List<String> topic1 = new ArrayList<>();
+        topic1.add("Topic1");
+        topic1.add("Topic2");
+        topic1.add("Topic3");
+
+        List<String> topic2 = new ArrayList<>();
+        topic2.add("Topic1");
+        topic2.add("Topic2");
+        topic2.add("Topic3");
+
+        List<String> topic3 = new ArrayList<>();
+        topic3.add("Topic1");
+        topic3.add("Topic2");
+        topic3.add("Topic3");
+
+        List<String> topic4 = new ArrayList<>();
+        topic4.add("Topic1");
+        topic4.add("Topic2");
+        topic4.add("Topic3");
+
+        List<String> topic5 = new ArrayList<>();
+        topic5.add("Topic1");
+        topic5.add("Topic2");
+        topic5.add("Topic3");
+
+
+        topicList.put(chapterList.get(0),topic1);
+        topicList.put(chapterList.get(1),topic2);
+        topicList.put(chapterList.get(2),topic3);
+        topicList.put(chapterList.get(3),topic4);
+        topicList.put(chapterList.get(4),topic5);
+
     }
 
     @Override
