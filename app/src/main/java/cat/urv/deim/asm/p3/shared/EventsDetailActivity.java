4d11f11c57af89e290808e313d41171c3f7e6b3d
pjuanma <63060206+pjuanma@users.noreply.github.com>
2020-05-16 23:56:47 +0200

    Ya esta por hoy

--- app/src/main/java/cat/urv/deim/asm/p3/shared/EventsDetailActivity.java
+++ app/src/main/java/cat/urv/deim/asm/p3/shared/EventsDetailActivity.java
@@ -1,10 +1,16 @@
 package cat.urv.deim.asm.p3.shared;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
 
+import cat.urv.deim.asm.p2.common.MainActivity;
 import cat.urv.deim.asm.p2.common.R;
+import cat.urv.deim.asm.p3.shared.faqs.FaqsActivity;
 
 public class EventsDetailActivity extends AppCompatActivity {
 
@@ -12,5 +18,29 @@ public class EventsDetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_events_detail);
+
+        Toolbar toolbar = findViewById(R.id.toolbar_events_detail);
+        setSupportActionBar(toolbar);
+
+        //El metodo setDiplayHomeUpEnabled pone la flecha pero al hacer click lo quita de cualquier elemento UI.
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        //Ponemos un listener en el objeto de la flecha hacia atras para que vaya un paso atras
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // perform whatever you want on back arrow click
+                Intent intent = new Intent(EventsDetailActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //Falta inflater de menú.
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_no_search, menu);
+        return true;
     }
 }
